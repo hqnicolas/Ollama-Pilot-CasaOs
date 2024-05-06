af31b25eb36d61e2b157b987e251e9f092bf588b
Nicolas Pereira <41456803+hqnicolas@users.noreply.github.com>
2024-05-06 12:38:46 -0300

    install miniconda

--- install_miniconda.sh
+++ install_miniconda.sh
@@ -4,3 +4,5 @@ wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/
 bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
 rm -rf ~/miniconda3/miniconda.sh
 ~/miniconda3/bin/conda init bash
+sudo apt-get install python-is-python3 -y
+sudo apt install python3.11-venv -y
